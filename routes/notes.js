072cde3099d6b90672d948fa7ae7dddea7dd8381
Amy Shafer <amy.u.shafer@gmail.com>
2022-03-28 15:56:25 -0400

    update for comments

--- routes/notes.js
+++ routes/notes.js
@@ -10,7 +10,6 @@ notes.get('/api/notes', (req, res) => {
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 
-// POST Route for submitting feedback
 notes.post('/api/notes', (req, res) => {
     const { title, text } = req.body;
 
